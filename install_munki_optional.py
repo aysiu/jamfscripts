be45318ed70e7e98d9e62853f6a3274245d904ae
aysiu <aysiu@users.noreply.github.com>
2020-05-18 08:34:59 -0700

    Create SelfServeManifest if it doesn't exist
    Users who've never installed any optional install ever won't have a SelfServeManifest

--- install_munki_optional.py
+++ install_munki_optional.py
@@ -15,6 +15,7 @@ import subprocess
 import sys
 
 def main():
+
     try:
         item_to_add = sys.argv[4]
     except:
@@ -27,7 +28,7 @@ def main():
     # Path to SelfServeManifest
     manifest_location = '/Library/Managed Installs/manifests/SelfServeManifest'
 
-    # Add the item as an optional install
+    # Add item as an optional install
     if os.path.isfile(manifest_location):
         # Try to read the current plist contents
         try:
@@ -40,64 +41,64 @@ def main():
         except:
             print("ERROR: cannot read plist contents from {}".format(manifest_location))
         f.close()
-        # Initialize test variable
-        contents_changed = 0
-        if 'managed_installs' not in manifest_contents.keys():
-            manifest_contents[ 'managed_installs' ] = [ item_to_add ]
-            contents_changed = 1
-        elif isinstance(manifest_contents[ 'managed_installs' ], list):
-            if item_to_add not in manifest_contents[ 'managed_installs' ]:
-                manifest_contents[ 'managed_installs' ].append(item_to_add)
-                contents_changed = 1
-        else:
-            print("ERROR: Managed Installs is not an array")
-            sys.exit(1)
-        # There's no point in writing back the changes to the plist
-        # if nothing actually changed, so double-check there were content changes 
-        if contents_changed == 1:
-            # Try to write the changes back
-            try:
-                f = open(manifest_location, 'wb')
-            except:
-                print("ERROR: cannot open {} for writing".format(manifest_location))
-                sys.exit(1)
-            try:
-                plistlib.dump(manifest_contents, f)
-            except:
-                print("ERROR: cannot write plist contents back to {}".format(manifest_location))
-                sys.exit(1)
-            f.close()
-
-        # Whether there are changes or not, we still want to launch up MSC so the user can see what's going on
-        # Get the currently logged in user
-        cmd = [ '/usr/bin/stat', '-f%Su',  '/dev/console' ]
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8')
-        out, err = p.communicate()
-        if err:
-            print("ERROR: Unable to determine currently logged in user")
-            sys.exit(1)
-        else:
-            current_user = str(out).strip()
-
-        # Open up MSC, so the user can see what's going on
-        if current_user not in ignored_users:
-            # If there were any changes, open up the updates
-            if contents_changed == 1:
-                open_munki = 'open munki://updates'
-            # If there were no changes, remind the user that this item is already installed
-            else:
-                open_munki = 'open munki://detail-' + item_to_add + '.html'
-            cmd = [ '/usr/bin/su', '-l', current_user, '-c', open_munki ]
-            subprocess.call(cmd)
-            # If there were any changes, also do an --auto run, so the item actually installs
-            if contents_changed == 1:
-                # Do a background Munki run
-                cmd = [ '/usr/local/munki/managedsoftwareupdate', '--auto' ]
-                subprocess.call(cmd)
-
     else:
-        print("ERROR: {} does not exist".format(manifest_location))
+        # If there is no SelfServeManifest file, we'll just end up creating it
+        manifest_contents = {}
+    # Initialize test variable
+    contents_changed = 0
+    if 'managed_installs' not in manifest_contents.keys():
+        manifest_contents[ 'managed_installs' ] = [ item_to_add ]
+        contents_changed = 1
+    elif isinstance(manifest_contents[ 'managed_installs' ], list):
+        if item_to_add not in manifest_contents[ 'managed_installs' ]:
+            manifest_contents[ 'managed_installs' ].append(item_to_add)
+            contents_changed = 1
+    else:
+        print("ERROR: Managed Installs is not an array")
         sys.exit(1)
+
+    # There's no point in writing back the changes to the plist
+    # if nothing actually changed, so double-check there were content changes 
+    if contents_changed == 1:
+        # Try to write the changes back
+        try:
+            f = open(manifest_location, 'wb')
+        except:
+            print("ERROR: cannot open {} for writing".format(manifest_location))
+            sys.exit(1)
+        try:
+            plistlib.dump(manifest_contents, f)
+        except:
+            print("ERROR: cannot write plist contents back to {}".format(manifest_location))
+            sys.exit(1)
+        f.close()
+
+    # Whether there are changes or not, we still want to launch up MSC so they can see what's going on
+    # Get the currently logged in user
+    cmd = [ '/usr/bin/stat', '-f%Su',  '/dev/console' ]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8')
+    out, err = p.communicate()
+    if err:
+        print("ERROR: Unable to determine currently logged in user")
+        sys.exit(1)
+    else:
+        current_user = str(out).strip()
+
+    # Open up MSC, so they can see what's going on
+    if current_user not in ignored_users:
+        # If there were any changes, open up the updates
+        if contents_changed == 1:
+            open_munki = 'open munki://updates'
+        # If there were no changes, remind the user that this item is already installed
+        else:
+            open_munki = 'open munki://detail-' + item_to_add + '.html'
+        cmd = [ '/usr/bin/su', '-l', current_user, '-c', open_munki ]
+        subprocess.call(cmd)
+        # If there were any changes, also do an --auto run, so the item actually installs
+        if contents_changed == 1:
+            # Do a background Munki run
+            cmd = [ '/usr/local/munki/managedsoftwareupdate', '--auto' ]
+            subprocess.call(cmd)
 
 if __name__ == "__main__":
     main()
